42eb5274b42efdaa5651361b5f302c1d4a484833
bakuchi- <helveticaf0015se@gmail.com>
2020-12-19 12:09:03 +0900

    fix

--- practice_program/second-term/packman/main.c
+++ practice_program/second-term/packman/main.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 
-//マップチップのデータの横幅
+//マップチップの横幅
 #define MAP_CHIP_WIDTH 21
-//マップチップのデータの縦幅
+//マップチップのの縦幅
 #define MAP_CHIP_HEIGHT 21
-#define ENEMY_NUMBER 3
-//表示する　文字　の設定。
-#define ITEM "\x1b[44m@\x1b[49m"
+#define ENEMY_NUMBER 5
+//表示するアイコン
+#define ITEM "\x1b[44m\x1b[49m"
 #define PLAYER "\x1b[44m  \x1b[49m"
 #define ENEMY "\x1b[41m  \x1b[49m"
 #define WALL "\x1b[49m  "
 #define INVISIBLE_WALL "\x1b[47m  \x1b[49m"
 #define ROAD "\x1b[47m  \x1b[49m"
 
-//マップチップのデータ。
 int map_chip_data[MAP_CHIP_HEIGHT][MAP_CHIP_WIDTH] =
     {
         {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
@@ -24,10 +23,10 @@ int map_chip_data[MAP_CHIP_HEIGHT][MAP_CHIP_WIDTH] =
         {2, 0, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 0, 2},
         {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
         {2, 0, 2, 0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0, 2, 0, 2},
-        {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 0, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
-        {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 0, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
-        {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 0, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
-        {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 0, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
+        {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 3, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
+        {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 3, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
+        {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 3, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
+        {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 3, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
         {2, 0, 2, 0, 2, 2, 0, 2, 3, 3, 0, 3, 3, 2, 0, 2, 2, 0, 2, 0, 2},
         {2, 0, 2, 0, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 0, 2, 0, 2},
         {2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2},
@@ -40,7 +39,7 @@ int map_chip_data[MAP_CHIP_HEIGHT][MAP_CHIP_WIDTH] =
         {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
         {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
 };
-// MapChip構造体の定義をする
+// MapChip構造体
 struct MapChip
 {
     char *icon;
@@ -54,6 +53,7 @@ enum EntityState
     None,
     Encount,
 };
+// アイコンの動く方向を示す
 enum MoveCommand
 {
     Up,
@@ -61,17 +61,23 @@ enum MoveCommand
     Right,
     Left
 };
+/**
+ * @struct Entity
+ * @brief プレイヤーや敵などの動く実体
+ */
 struct Entity
 {
     char *icon;
     unsigned int position_x;
     unsigned int position_y;
-    unsigned int velocity;
     unsigned int velocity_x;
     unsigned int velocity_y;
     enum MoveCommand command;
     enum EntityState state;
 };
+/** @fn
+ * @brief mapchipの初期化
+ */
 void initMapChip()
 {
     for (unsigned int i = 0; i < MAP_CHIP_HEIGHT; i++)
@@ -96,6 +102,30 @@ void initMapChip()
         }
     }
 }
+/** @fn 
+ * @brief 敵の初期化
+ * @param enemies 敵のポインタ配列
+ * @param number 作る敵の数
+ */
+void initEnemy(struct Entity *enemies[], int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        struct Entity *enemy = (struct Entity *)malloc(sizeof(struct Entity));
+        enemies[i] = enemy;
+        enemies[i]->icon = ENEMY;
+        enemies[i]->position_x = MAP_CHIP_WIDTH / 2;
+        enemies[i]->position_y = MAP_CHIP_HEIGHT / 2;
+        enemies[i]->velocity_x = 0;
+        enemies[i]->velocity_y = 0;
+    }
+    enemies[number] = (struct Entity *)NULL;
+}
+/** @fn　
+ * @brief エンティティの動く方向をセットする
+ * @param entity エンティティ構造体
+ * @param mvcommand コマンド文字列
+ */
 void setDirection(struct Entity *entity, char mvcommand)
 {
     // char mvcommand;
@@ -125,7 +155,56 @@ void setDirection(struct Entity *entity, char mvcommand)
         entity->command = Left;
     }
 }
-void entityMove(struct Entity *entity, char mvcommand)
+/** @fn
+ * @brief ランダムなコマンド文字生成
+ * @return コマンド文字
+ */
+char genRndCmd()
+{
+    char mvcommand;
+    int rnd = (float)rand() / (RAND_MAX + 1.0) * 4.0; /* generate random values */
+    if (rnd == 1)
+        mvcommand = 'f';
+    if (rnd == 2)
+        mvcommand = 'e';
+    if (rnd == 3)
+        mvcommand = 'd';
+    if (rnd == 4)
+        mvcommand = 's';
+    return mvcommand;
+}
+/** @fn
+ * @param entity エンティティ構造体
+ * @param chip MapChip構造体
+ */
+int entityMoveCheck(struct Entity *entity, struct MapChip chip[MAP_CHIP_HEIGHT][MAP_CHIP_WIDTH])
+{
+    // printf("chip.id %d", chip.id);
+    // printf("chip[entity->position_y][entity->position_x].position_y %d\n", chip[entity->position_y][entity->position_x].position_y);
+    // printf("chip[entity->position_y][entity->position_x].position_x %d\n", chip[entity->position_y][entity->position_x].position_x);
+    // printf("entity->position_y %d\n", entity->position_y);
+    // printf("dentity->position_x %d\n", entity->position_x);
+    // printf("chip.icon  %s\n", chip[entity->position_y][entity->position_x].icon);
+
+    int cond1 = entity->position_x == chip[entity->position_y][entity->position_x].position_x;
+    int cond2 = entity->position_y == chip[entity->position_y][entity->position_x].position_y;
+    // エンティティの座標の位置は歩行可能なエリアであるならばreturn 1
+    // そうでないなら，entityの座標を一個戻す
+    if (cond1 && cond2 && (chip[entity->position_y][entity->position_x].id != 0))
+    {
+        entity->position_x -= entity->velocity_x;
+        entity->position_y -= entity->velocity_y;
+        return 0;
+    }
+    return 1;
+}
+/** @fn
+ * @brief Entityを指定されたコマンドに従った方向に動かす
+ * @param entity エンティティ構造体ポインタ
+ * @param chip MapChip構造体
+ * @param　mvcommand コマンドもじ
+ */
+int entityMove(struct Entity *entity, struct MapChip chip[MAP_CHIP_HEIGHT][MAP_CHIP_WIDTH], char mvcommand)
 {
     char directions[4] = {'f', 'e', 'd', 's'};
     setDirection(entity, mvcommand);
@@ -140,7 +219,13 @@ void entityMove(struct Entity *entity, char mvcommand)
         entity->position_y = MAP_CHIP_HEIGHT - 2;
     if (entity->position_y == 0)
         entity->position_y = 1;
+    return entityMoveCheck(entity, map_chip);
 }
+/** @fn
+ * 与えられたentityの構造体の座標が一致しているならば1そうでなければ0を返す
+ *  @param player エンティティ構造体
+ *  @param enemy エンティティ構造体
+ */
 int isMatchPosition(struct Entity *player, struct Entity *enemy)
 {
     if (player->position_x == enemy->position_x && player->position_y == enemy->position_y)
@@ -150,37 +235,29 @@ int isMatchPosition(struct Entity *player, struct Entity *enemy)
     }
     return 0;
 }
-void initEnemy(struct Entity *enemies[], int number)
-{
-    for (int i = 0; i < number; i++)
-    {
-        struct Entity *enemy = (struct Entity *)malloc(sizeof(struct Entity));
-        enemies[i] = enemy;
-        enemies[i]->icon = ENEMY;
-        enemies[i]->position_x = MAP_CHIP_WIDTH / 2;
-        enemies[i]->position_y = MAP_CHIP_HEIGHT / 2;
-        enemies[i]->velocity_x = 0;
-        enemies[i]->velocity_y = 1;
-    }
-    enemies[number] = (struct Entity *)NULL;
-}
-void enemiesMove(struct Entity *enemies[])
+/** @fn
+ * @brief 複数の敵を動かす
+ * @param enemies エンティティ構造体のポインタ配列
+ * @param chip MapChip構造体
+ */
+void enemiesMove(struct Entity *enemies[], struct MapChip chip[MAP_CHIP_HEIGHT][MAP_CHIP_WIDTH])
 {
     for (int i = 0; enemies[i] != NULL; i++)
     {
-        char mvcommand;
-        int rnd = (float)rand() / (RAND_MAX + 1.0) * 4.0; /* generate random values */
-        if (rnd == 1)
-            mvcommand = 'f';
-        if (rnd == 2)
-            mvcommand = 'e';
-        if (rnd == 3)
-            mvcommand = 'd';
-        if (rnd == 4)
-            mvcommand = 's';
-        entityMove(enemies[i], mvcommand);
+        char cmd; //コマンド文字
+        // 動かした先が歩行可能エリアでないならばやり直す．
+        do
+        {
+            //random生成
+            cmd = genRndCmd();
+        } while (!entityMove(enemies[i], chip, cmd));
     }
 }
+/** @fn
+ * @brief ゲームオーバーならば1を返す
+ * @param enemies エンティティ構造体のポインタ配列
+ * @param player エンティティ構造体
+ */
 int isGameOver(struct Entity *enemies[], struct Entity *player)
 {
     for (int i = 0; enemies[i] != NULL; i++)
@@ -190,6 +267,11 @@ int isGameOver(struct Entity *enemies[], struct Entity *player)
     }
     return 0;
 }
+/** @fn
+ * @param enemies エンティティ構造体のポインタ配列
+ * @param x 座標
+   @param y 座標
+ */
 int printEnemies(struct Entity *enemies[], int x, int y)
 {
     for (int i = 0; enemies[i] != NULL; i++)
@@ -202,32 +284,6 @@ int printEnemies(struct Entity *enemies[], int x, int y)
     }
     return 0;
 }
-int entityMoveCheck(struct Entity *entity, struct MapChip chip[MAP_CHIP_HEIGHT][MAP_CHIP_WIDTH])
-{
-    // printf("chip.id %d", chip.id);
-    int cond1 = entity->position_x == chip[entity->position_y][entity->position_x].position_x;
-    int cond2 = entity->position_y == chip[entity->position_y][entity->position_x].position_y;
-    // printf("chip[entity->position_y][entity->position_x].position_y %d\n", chip[entity->position_y][entity->position_x].position_y);
-    // printf("chip[entity->position_y][entity->position_x].position_x %d\n", chip[entity->position_y][entity->position_x].position_x);
-    // printf("entity->position_y %d\n", entity->position_y);
-    // printf("dentity->position_x %d\n", entity->position_x);
-    // printf("chip.icon  %s\n", chip[entity->position_y][entity->position_x].icon);
-    if ((chip[entity->position_y][entity->position_x].icon != ROAD) && cond1 && cond2)
-    {
-        entity->position_x -= entity->velocity_x;
-        entity->position_y -= entity->velocity_y;
-        return 0;
-    }
-    return 1;
-    // if (chip.id == 0)
-    //     return;
-    // if (entity->position_x == chip.position_x && entity->position_y == chip.position_y)
-    // {
-    //     entity->position_x -= entity->velocity_x;
-    //     entity->position_y -= entity->velocity_y;
-    //     return;
-    // }
-}
 int main()
 {
     struct Entity *enemies[ENEMY_NUMBER];
@@ -238,20 +294,18 @@ int main()
     player.velocity_x = 0;
     player.velocity_y = 1;
     initMapChip();
-    initEnemy(enemies, ENEMY_NUMBER - 1);
+    initEnemy(enemies, ENEMY_NUMBER);
     do
     {
         char mvcommand;
         scanf(" %c", &mvcommand);
-        entityMove(&player, mvcommand);
-        enemiesMove(enemies);
-        entityMoveCheck(&player, map_chip);
+        entityMove(&player, map_chip, mvcommand);
+        enemiesMove(enemies, map_chip);
+        // entityMoveCheck(&player, map_chip);
         for (unsigned int y = 0; y < MAP_CHIP_HEIGHT; y++)
         {
             for (unsigned int x = 0; x < MAP_CHIP_WIDTH; x++)
             {
-
-                ///@todo プレイヤーの移動ができるかどうか
                 if (player.position_y == y && player.position_x == x)
                 {
                     printf("%s", player.icon);
